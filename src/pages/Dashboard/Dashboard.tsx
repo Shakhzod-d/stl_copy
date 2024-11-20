553490ab44d0a430a8edbc6c9301c03f7f9ae735
ergashelmonov <elmonovergash@gmail.com>
2024-11-20 16:28:44 +0500

    fixed timepicker btn

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -91,9 +91,9 @@ export const Dashboard = () => {
       <Navbar title="Dashboard" />
       <Day>
         <Flex $gap={"20px"} style={{ position: "relative" }}>
-          {open && <TimePickerModal setOpen={setOpen} /> }
-          <CustomBtn>
-            <BiCalendarStar size={30} onClick={() => setOpen(true)} />
+          {open && <TimePickerModal setOpen={setOpen} />}
+          <CustomBtn onClick={() => setOpen(true)}>
+            <BiCalendarStar size={30} />
           </CustomBtn>
         </Flex>
         <Flex $gap={"5px"}>
