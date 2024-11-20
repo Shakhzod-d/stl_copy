fd0bbe802b06925795aaee63652114a319fc0ebf
ergashelmonov <elmonovergash@gmail.com>
2024-11-20 19:43:17 +0500

    time picker theme fixed

--- src/track/components/shared/time-picker/styled.ts
+++ src/track/components/shared/time-picker/styled.ts
@@ -2,24 +2,28 @@ import { Calendar } from "antd";
 import styled from "styled-components";
 
 export const StyledCalendar = styled(Calendar)`
-  border-bottom: 1px solid #c7c5c5;
+  border-bottom: 1px solid ${({ theme }) => theme.clrGray2};
 
   thead {
     opacity: 0.5 !important;
   }
+
   * {
     border: 0 !important;
     font-size: 20px !important;
     font-weight: 400 !important;
-    line-height: 25px !important; /* 125% */
+    line-height: 25px !important;
     letter-spacing: -0.4px !important;
+    color: ${({ theme }) => theme.clr} !important;
   }
 
   .ant-picker-calendar-header {
     justify-content: start !important;
     flex-direction: row-reverse !important;
+    background: ${({ theme }) => theme.white} !important;
 
     .ant-select-selector {
+      background: ${({ theme }) => theme.white} !important;
       padding: 0 !important;
       gap: 0 !important;
     }
@@ -27,20 +31,28 @@ export const StyledCalendar = styled(Calendar)`
     .ant-select-arrow {
       display: none !important;
     }
+
     * {
-      color: #000 !important;
+      color: ${({ theme }) => theme.clr} !important;
       font-size: 20px !important;
       font-weight: 600 !important;
-      line-height: 22px !important; /* 129.412% */
+      line-height: 22px !important;
       letter-spacing: -0.5px !important;
     }
+
     *::after {
       display: none !important;
     }
   }
+
   .ant-picker-calendar-mode-switch {
     display: none !important;
   }
+
+  .ant-picker-panel {
+    background: ${({ theme }) => theme.white} !important;
+  }
+
   .ant-picker-calendar-date {
     width: 44px !important;
     height: 44px !important;
@@ -48,7 +60,12 @@ export const StyledCalendar = styled(Calendar)`
     display: flex !important;
     justify-content: center !important;
     align-items: center !important;
+    background: ${({ theme }) => theme.textAriaBg};
+    &:hover {
+      background: transparent !important;
+    }
   }
+
   .ant-picker-cell-today {
     * {
       color: #fc973a !important;
@@ -69,16 +86,16 @@ export const StyledCalendar = styled(Calendar)`
 `;
 
 export const P = styled.p`
-  color: #000;
+  color: ${({ theme }) => theme.clr};
   font-size: 17px;
   font-weight: 400;
-  line-height: 22px; /* 129.412% */
+  line-height: 22px;
   letter-spacing: -0.4px;
 `;
 
 export const Div = styled.div`
   border-radius: 9px;
-  background: #f9f9fa;
+  background: ${({ theme }) => theme.textAriaBg};
   display: flex;
   align-items: center;
 `;
@@ -93,6 +110,7 @@ export const Modal = styled.div`
   flex-direction: column;
   gap: 10px;
   border-radius: 10px;
-  background: #fff;
+  background: ${({ theme }) => theme.white};
   padding: 10px 16px;
+  box-shadow: ${({ theme }) => theme.selectShadow};
 `;
